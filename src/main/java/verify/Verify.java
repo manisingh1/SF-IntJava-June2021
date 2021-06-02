28812883d010823eb83e2d20ceef0d297a1c73e5
simon <simon@dancingcloudservices.com>
2021-06-02 16:59:33 -0600

    Day 1 Changed to Java 11

--- src/main/java/verify/Verify.java
+++ src/main/java/verify/Verify.java
@@ -2,7 +2,7 @@ package verify;
 
 public record Verify(int x, String y) {
   public static void main(String[] args) {
-    Object v = new Verify(10, "Hello");
-    System.out.println(v instanceof Verify t && t.x == 10);
+//    Object v = new Verify(10, "Hello");
+//    System.out.println(v instanceof Verify t && t.x == 10);
   }
 }
